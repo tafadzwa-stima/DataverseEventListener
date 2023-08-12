e84dcca83a7552b05bd040650a96bb93d5014542
tafadzwa-stima <129663766+tafadzwa-stima@users.noreply.github.com>
2023-08-12 23:44:26 +0200

    Add files via upload
    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataverseEventListener
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
