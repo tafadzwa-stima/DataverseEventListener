c6c5b08cde15a797c10ff30766c8aaa7590ed39a
Tafadzwa Stima <Tafadzwa.Stima@investec.co.za>
2023-08-13 15:05:49 +0200

    set up a a queueclient and recieved the message

--- Program.cs
+++ Program.cs
@@ -1,10 +1,42 @@
-﻿namespace DataverseEventListener
+﻿using Azure.Messaging.ServiceBus;
+
+namespace DataverseEventListener
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void  Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GetMessage();
+
+
+            Console.ReadLine();
+        }
+        public static async Task GetMessage()
+        {
+            string connectionString = "Endpoint=sb://powerplatformnamespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=DrYVlJn7ZbS2rUTszn/R3y74dB04Q71rm+ASbEi2z3U=";
+            string queueName = "powerplatformqueue";
+
+           // RemoteExecutionContext context = null;
+
+            //this is how you create a Queueclient with the new AzureService sdk
+            var client = new ServiceBusClient(connectionString);
+
+            var receiver = client.CreateReceiver(queueName);
+
+            var receivedMessage = await receiver.ReceiveMessageAsync();
+
+            if (receivedMessage.ContentType == "application/json")
+            {
+                Console.WriteLine("Message is JSON");
+            }
+            else { Console.WriteLine("message is not json"); }
+
+            string body = receivedMessage.Body.ToString();
+
+            Console.WriteLine(body);
+            Console.ReadKey();
+
+
         }
     }
 }
---
+++ ReceiveMessage.cs
@@ -0,0 +1,40 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataverseEventListener
+{
+    public class ReceiveMessage
+    {
+        public static async Task GetMessage()
+        {
+            string connectionString = "Endpoint=sb://powerplatformnamespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=DrYVlJn7ZbS2rUTszn/R3y74dB04Q71rm+ASbEi2z3U=";
+            string queueName = "powerplatformqueue";
+
+            RemoteExecutionContext context = null;
+
+
+            var client = new ServiceBusClient(connectionString);
+
+            ServiceBusReceiver receiver = client.CreateReceiver(queueName);
+
+            ServiceBusReceivedMessage receivedMessage = await receiver.ReceiveMessageAsync();
+
+            if (receivedMessage.ContentType =="application/json")
+            {
+                Console.WriteLine("Message is JSON");
+            }
+            else { Console.WriteLine("message is not json") ; }
+
+            string body = receivedMessage.Body.ToString();
+
+            Console.WriteLine(body);
+           
+
+        }
+    }
+}
